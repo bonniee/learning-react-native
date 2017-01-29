df24041507bdc5682e021005817f64823ddbbe2c
Bonnie Eisenman <bonnie.eisenman@gmail.com>
2017-01-29 15:17:09 -0500

    fix some of the rendering bugs

--- WeatherProject/index.ios.js
+++ WeatherProject/index.ios.js
@@ -3,4 +3,4 @@ import { AppRegistry } from "react-native";
 import WeatherProjectPart1 from "./WeatherProject_Part1";
 import WeatherProject from "./WeatherProject";
 
-AppRegistry.registerComponent("WeatherProject", () => WeatherProjectPart1);
+AppRegistry.registerComponent("WeatherProject", () => WeatherProject);
