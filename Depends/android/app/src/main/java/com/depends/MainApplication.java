705e0d17facb9fd642b616cf9b0d4b583eaea30c
Bonnie Eisenman <beisenman@twitter.com>
2017-02-12 21:47:28 -0500

    hello world android

--- Depends/android/app/src/main/java/com/depends/MainApplication.java
+++ Depends/android/app/src/main/java/com/depends/MainApplication.java
@@ -5,6 +5,7 @@ import android.util.Log;
 
 import com.facebook.react.ReactApplication;
 import com.brentvatne.react.ReactVideoPackage;
+import com.depends.helloworld.*;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -26,7 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new ReactVideoPackage()
+            new ReactVideoPackage(),
+            new HelloWorldPackage()
       );
     }
   };
