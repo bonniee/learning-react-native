a659ec6ab1291ba284daa0b98896fa8d3441d644
Bonnie Eisenman <beisenman@twitter.com>
2017-02-16 16:43:10 -0500

    prettier

--- Touch/PressDemo.js
+++ Touch/PressDemo.js
@@ -1,10 +1,5 @@
-import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableHighlight
-} from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, TouchableHighlight } from "react-native";
 
 class Button extends Component {
   constructor(props) {
@@ -13,12 +8,12 @@ class Button extends Component {
   }
 
   _onPressIn = () => {
-    this.setState({pressing: true});
-  }
+    this.setState({ pressing: true });
+  };
 
   _onPressOut = () => {
-   this.setState({pressing: false}); 
-  }
+    this.setState({ pressing: false });
+  };
 
   render() {
     return (
@@ -26,44 +21,44 @@ class Button extends Component {
         <TouchableHighlight
           onPressIn={this._onPressIn}
           onPressOut={this._onPressOut}
-          style={styles.touchable}>
+          style={styles.touchable}
+        >
 
           <View style={styles.button}>
             <Text style={styles.welcome}>
-              {this.state.pressing ? 'EEK!' : 'PUSH ME'}
+              {this.state.pressing ? "EEK!" : "PUSH ME"}
             </Text>
           </View>
 
         </TouchableHighlight>
       </View>
-      );
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
+    textAlign: "center",
     margin: 10,
-    color: '#FFFFFF'
+    color: "#FFFFFF"
   },
   touchable: {
     borderRadius: 100
   },
   button: {
-    backgroundColor: '#FF0000',
+    backgroundColor: "#FF0000",
     borderRadius: 100,
     height: 200,
     width: 200,
-    justifyContent: 'center'
-  },
+    justifyContent: "center"
+  }
 });
-
 
 export default Button;
--- Touch/index.ios.js
+++ Touch/index.ios.js
@@ -1,9 +1,7 @@
-import React from 'react';
-import {
-  AppRegistry,
-} from 'react-native';
+import React from "react";
+import { AppRegistry } from "react-native";
 
-import PressDemo from './PressDemo';
-import PanDemo from './PanDemo';
+import PressDemo from "./PressDemo";
+import PanDemo from "./PanDemo";
 
-AppRegistry.registerComponent('Touch', () => PanDemo);
+AppRegistry.registerComponent("Touch", () => PanDemo);
