ab9300b90b6781474266bcaa1affb06e0a3d7caa
Bonnie Eisenman <bonnie.eisenman@gmail.com>
2017-06-24 17:31:10 -0400

    Make it work on android

--- Depends/android/app/src/main/java/com/depends/MainApplication.java
+++ Depends/android/app/src/main/java/com/depends/MainApplication.java
@@ -9,6 +9,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.depends.helloworld.HelloWorldPackage;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -24,7 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new ReactVideoPackage()
+          new ReactVideoPackage(),
+          new HelloWorldPackage()
       );
     }
   };
