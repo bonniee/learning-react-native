262898e531a94d000778545489557966fc473b91
Bonnie Eisenman <bonnie.eisenman@gmail.com>
2017-07-01 16:28:15 -0400

    add crossplatform CRNA app

---
+++ crossplatform/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Newsflash from './Newsflash';
+
+export default Newsflash;
---
+++ crossplatform/Newsflash.android.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+
+export default class App extends React.Component {
+  componentDidMount() {
+    Alert.alert("Hey!", "You're on Android.");
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          What? I didn't say anything.
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
