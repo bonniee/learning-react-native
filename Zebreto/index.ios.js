a45d69ec36d747e02478064f698a1409d62dcc55
Bonnie Eisenman <bonnie.eisenman@gmail.com>
2017-07-05 17:45:21 -0400

    Delete old Zebreto project

--- Zebreto/index.ios.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-} from 'react-native';
-
-import Zebreto from './src/components/Zebreto';
-
-AppRegistry.registerComponent('Zebreto', () => Zebreto);
