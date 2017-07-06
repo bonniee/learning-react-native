b2349ec03e676ff97dd3933321bfd78742bf1d1a
Bonnie Eisenman <beisenman@twitter.com>
2017-07-06 11:37:43 -0400

    fix styles project

---
+++ src/styles/__tests__/FlexDemo-test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import FlexDemo from "../FlexDemo";
+
+import renderer from "react-test-renderer";
+
+test('renders correctly', () => {
+  const tree = renderer.create(
+    <FlexDemo />
+    ).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
