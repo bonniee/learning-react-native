efcd0c04f2a0d936a28a34f765d683d92c236c09
Bonnie Eisenman <bonnie.eisenman@gmail.com>
2017-07-22 11:30:18 -0400

    add third checkpoint

---
+++ src/weather/WeatherProject_Part3.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import OpenWeatherMap from "./open_weather_map";
+import Forecast from "./Forecast";
+
+class WeatherProject extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { zip: "", forecast: null };
+  }
+
+  _handleTextChange = event => {
+    let zip = event.nativeEvent.text;
+    OpenWeatherMap.fetchForecast(zip).then(forecast => {
+      this.setState({ forecast: forecast });
+    });
+  };
+
+  render() {
+    let content = null;
+    if (this.state.forecast !== null) {
+      content = (
+        <Forecast
+          main={this.state.forecast.main}
+          description={this.state.forecast.description}
+          temp={this.state.forecast.temp}
+        />
+      );
+    }
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          You input {this.state.zip}.
+        </Text>
+        {content}
+        <TextInput
+          style={styles.input}
+          onSubmitEditing={this._handleTextChange}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#666666"
+  },
+  welcome: { fontSize: 20, textAlign: "center", margin: 10 },
+  input: {
+    fontSize: 20,
+    borderWidth: 2,
+    padding: 2,
+    height: 40,
+    width: 100,
+    textAlign: "center"
+  }
+});
+
+export default WeatherProject;
--- src/weather/index.js
+++ src/weather/index.js
@@ -1,3 +1,4 @@
+import WeatherProjectPart3 from "./WeatherProject_Part3";
 import WeatherProjectPart2 from "./WeatherProject_Part2";
 import WeatherProjectPart1 from "./WeatherProject_Part1";
 import WeatherProject from "./WeatherProject";
