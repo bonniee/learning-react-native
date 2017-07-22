967921a8bad1f267a11d0fc57eb6feb93e012557
Bonnie Eisenman <bonnie.eisenman@gmail.com>
2017-07-22 13:10:03 -0400

    update patches

---
+++ src/flashcards/.make-patches.sh
@@ -0,0 +1,3 @@
+git diff --no-index src_checkpoint_01 src_checkpoint_02 > patches/01_to_02_navigator.patch
+git diff --no-index src_checkpoint_02 src_checkpoint_03 > patches/02_to_03_redux.patch
+git diff --no-index src_checkpoint_03 src_checkpoint_04 > patches/03_to_04_storage.patch
